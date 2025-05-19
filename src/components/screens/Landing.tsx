99a79f2c39a9f5cd926d99faa613aa5e93694edd
irolinski <ignacy.rolinski@gmail.com>
2025-05-19 15:29:43 +0200

    feat(LandingPage): add responsiveness for small & medium screens

--- src/components/screens/Landing.tsx
+++ src/components/screens/Landing.tsx
@@ -2,43 +2,57 @@ import Image from "next/image";
 
 const LandingScreen = () => {
   return (
-    <div className="landing-screen section">
-      <div className="slogan">
-        <h2 className="slogan-first-part">CBT that fits in your pocket -</h2>
-        <h2 className="slogan-second-part">and your routine</h2>
-      </div>
-      <Image
-        className="relax-image"
-        src={"/images/relax-banner.webp"}
-        alt={
-          "Promotional banner with a man lying on a hammock that looks like WorryFree logo."
-        }
-        width={0}
-        height={0}
-        sizes="100%"
-        style={{ width: "80%", height: "auto" }}
-      />
-      <div className="store-links-container">
-        <button className="store-link-button">
+    <div className="section">
+      <div className="landing-screen ">
+        <div className="content">
+          <div className="slogan slogan-original-container">
+            <h2 className="slogan-first-part">
+              CBT that fits in your pocket{" "}
+              <span className="slogan-hyphen">-</span>
+            </h2>
+            <h2 className="slogan-second-part">and your routine.</h2>
+          </div>
           <Image
-            src={"/images/app-store-button.webp"}
-            alt={"App Store hyperlink button"}
+            className="relax-image"
+            src={"/images/relax-banner.webp"}
+            alt={
+              "Promotional banner with a man lying on a hammock that looks like WorryFree logo."
+            }
             width={0}
             height={0}
             sizes="100%"
-            style={{ width: "100%", height: "auto" }}
+            style={{ width: "80%", height: "auto" }}
           />
-        </button>
-        <button className="store-link-button">
-          <Image
-            src={"/images/google-play-button.webp"}
-            alt={"Google Play Store hyperlink button"}
-            width={0}
-            height={0}
-            sizes="100%"
-            style={{ width: "100%", height: "auto" }}
-          />
-        </button>
+          <div className="slogan slogan-split-container">
+            <h2 className="slogan-second-part slogan-second-part-split">
+              and your routine.
+            </h2>
+          </div>
+          <div className="store-links-wrapper">
+            <div className="store-links-container">
+              <button className="store-link-button">
+                <Image
+                  src={"/images/app-store-button.webp"}
+                  alt={"App Store hyperlink button"}
+                  width={0}
+                  height={0}
+                  sizes="100%"
+                  style={{ width: "100%", height: "auto" }}
+                />
+              </button>
+              <button className="store-link-button">
+                <Image
+                  src={"/images/google-play-button.webp"}
+                  alt={"Google Play Store hyperlink button"}
+                  width={0}
+                  height={0}
+                  sizes="100%"
+                  style={{ width: "100%", height: "auto" }}
+                />
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
